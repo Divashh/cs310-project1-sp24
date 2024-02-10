2a168d46bbb79976c1a8a1111ca5d6afebf23cbf
Divash <dpokharel@stu.jsu.edu>
2024-02-10 12:52:16 -0600

    Project 1 Completed.

--- src/edu/jsu/mcis/cs310/ClassSchedule.java
+++ src/edu/jsu/mcis/cs310/ClassSchedule.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Arrays;
+import java.io.IOException;
 
 public class ClassSchedule {
     
@@ -33,14 +35,180 @@ public class ClassSchedule {
     private final String SUBJECTID_COL_HEADER = "subjectid";
     
     public String convertCsvToJsonString(List<String[]> csv) {
+        // Create "Outer" JSON Container
         
-        return ""; // remove this!
+        JsonObject json = new JsonObject();
+        
+        //Create "inner" JSON Containers
+        
+        JsonObject scheduletype = new JsonObject();
+        JsonObject subject = new JsonObject();
+        JsonObject course = new JsonObject();
+        ArrayList<JsonObject> section = new ArrayList<>();
+        
+        //Set up CSV iterator and then get reader row
+        
+        Iterator<String[]> iterator =csv.iterator();
+        String[] headerRow =iterator.next();
+        
+        HashMap<String, Integer> headers = new HashMap<>();
+        for (int i =0; i<headerRow.length; ++i){
+            headers.put(headerRow[i], i);
+        }
+        
+        //Declare the json string
+        String jsonstring = "";
+        
+        //Process CSV records
+        
+        while (iterator.hasNext()){
+            String[] record  =iterator.next();
+            
+            //Get the values from the record.
+           
+            Integer crn = Integer.valueOf(record[headers.get(CRN_COL_HEADER)]);
+            Integer credits = Integer.valueOf(record[headers.get(CREDITS_COL_HEADER)]);
+            
+            //TAKE num as string to split the course and its number.
+            String num = record[headers.get(NUM_COL_HEADER)];
+            
+            //Lets split them.
+            String[] parts = num.split(" ");
+            String COURSE = parts[0];
+            String COURSENUM = parts[1];
+            
+            //Also lets separate the instructors.
+            List<String> instructors = Arrays.asList(record[headers.get(INSTRUCTOR_COL_HEADER)].split(", "));
+            
+            
+            //Now we can fill up the inner hashmap of the section array.
+            JsonObject InSection = new JsonObject();
+            InSection.put(CRN_COL_HEADER, crn);
+            InSection.put(SUBJECTID_COL_HEADER,COURSE);
+            InSection.put(NUM_COL_HEADER,COURSENUM);
+            InSection.put(SECTION_COL_HEADER,record[headers.get(SECTION_COL_HEADER)]);
+            InSection.put(TYPE_COL_HEADER,record[headers.get(TYPE_COL_HEADER)]);
+            InSection.put(START_COL_HEADER,record[headers.get(START_COL_HEADER)]);
+            InSection.put(END_COL_HEADER,record[headers.get(END_COL_HEADER)]);
+            InSection.put(DAYS_COL_HEADER,record[headers.get(DAYS_COL_HEADER)]);
+            InSection.put(END_COL_HEADER,record[headers.get(END_COL_HEADER)]);
+            InSection.put(WHERE_COL_HEADER,record[headers.get(WHERE_COL_HEADER)]);
+            InSection.put(INSTRUCTOR_COL_HEADER,instructors);
+            
+            //Lets add these inner section hashmaps to the main outer section array.
+            section.add(InSection);
+            
+            //Now we move on to the inner course hashmap and we will fill it up.
+            JsonObject InCourse = new JsonObject();
+            InCourse.put(SUBJECTID_COL_HEADER, COURSE);
+            InCourse.put(NUM_COL_HEADER,COURSENUM);
+            InCourse.put(DESCRIPTION_COL_HEADER,record[headers.get(DESCRIPTION_COL_HEADER)]);
+            InCourse.put(CREDITS_COL_HEADER, credits);
+            
+            
+            //Now we can actually insert this inner course hashmap info to outer course hashmap
+            course.put(record[headers.get(NUM_COL_HEADER)], InCourse);
+            
+            // Lets fill up the subject hashmap
+            subject.put(COURSE,record[headers.get(SUBJECT_COL_HEADER)]);
+            
+            //scheduletype hashmap fillup
+            scheduletype.put(record[headers.get(TYPE_COL_HEADER)],record[headers.get(SCHEDULE_COL_HEADER)]);
+            
+            //Lets fill the outermost hashmaps with all the hashmaps
+            json.put("scheduletype", scheduletype);
+            json.put("subject", subject);
+            json.put("course", course);
+            json.put("section", section);
+            
+            // Serialize the json object to json string
+            jsonstring = Jsoner.serialize(json);
+            
+         }
+        
+        return jsonstring;
         
     }
     
     public String convertJsonToCsvString(JsonObject json) {
         
-        return ""; // remove this!
+        // Lets get the objects from JSON first
+        
+        JsonObject scheduletype = (JsonObject)json.get("scheduletype");
+        JsonObject subjects = (JsonObject)json.get("subject");
+        JsonObject course = (JsonObject)json.get("course");
+        JsonArray section = (JsonArray)json.get("section");
+        
+        // Lets declare the csv string
+        
+        String csvString ="";
+        
+        //We need to write in CSV format using CSv string
+        
+        try (StringWriter writer = new StringWriter();
+            CSVWriter csvwriter = new CSVWriter(writer,'\t', '"', '\\', "\n" )){
+            
+            //Lets put in the headers for the csv file
+            
+            csvwriter.writeNext(new String[]{"crn", "subject", "num", "description", "section", "type", "credits", "start", "end", "days", "where", "schedule", "instructor"});
+            
+            //Now we need a iterator to construct the csv rows
+            
+            for(Object Obj:section ){
+                JsonObject sections =(JsonObject) Obj;
+                
+                //Extract the data from Json objects
+                String crn = String.valueOf(sections.get(CRN_COL_HEADER));
+                String subjID = (String)sections.get(SUBJECTID_COL_HEADER);
+                String subject= (String)subjects.get(subjID);
+                String get_Num = (String)sections.get(NUM_COL_HEADER);
+                String num = subjID + " " + get_Num;
+                
+                JsonObject incourse = (JsonObject)course.get(num);
+                
+                // Now get the incourse data
+                
+                String description = (String)incourse.get(DESCRIPTION_COL_HEADER);
+                String sectionId = (String)sections.get(SECTION_COL_HEADER);
+                String type = (String)sections.get(TYPE_COL_HEADER);
+                String credits = String.valueOf(incourse.get(CREDITS_COL_HEADER));
+                String start = (String)sections.get(START_COL_HEADER);
+                String end = (String)sections.get(END_COL_HEADER);
+                String days = (String)sections.get(DAYS_COL_HEADER);
+                String where = (String)sections.get(WHERE_COL_HEADER);
+                String schedule = (String)scheduletype.get(type);
+                
+                //Get the instructpr details now
+                JsonArray instructorss = (JsonArray)sections.get(INSTRUCTOR_COL_HEADER);
+                List<String> instructorssList = new ArrayList<>();
+                for (Object instructors: instructorss){
+                    instructorssList.add(instructors.toString());
+                }
+                
+                String instructor = String.join(", ", instructorssList);
+                
+                
+                //Lets write the CSV row now
+                
+                csvwriter.writeNext(new String[]{crn, subject, num, description, sectionId, type, credits, start, end, days, where, schedule, instructor});
+                
+                
+            }
+            
+            //COnverting CSV writer to string
+            csvString = writer.toString();
+            
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        
+                
+            
+                    
+        
+        
+        
+        return csvString; 
         
     }
     
